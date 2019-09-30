84329ad2b19ef830b4cb4aeaaca47caef3020f12
maricorla <55948662+maricorla@users.noreply.github.com>
2019-09-30 22:54:40 +0300

    Create userInputs.java

---
+++ userInputs.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) {
+	
+	Scanner input = new Scanner(System.in);
+	System.out.println("Entered here your name: ");
+	String line = input.nextLine();
+	System.out.println("Your name is " + line);
+
+	}
+
+}
