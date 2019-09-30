faf92f146d7aaad76dfa320c8dce7fb3a93d02f7
maricorla <55948662+maricorla@users.noreply.github.com>
2019-09-30 18:33:58 +0300

    Create whileLoop.java

---
+++ whileLoop.java
@@ -0,0 +1,12 @@
+public class Application {
+
+	public static void main(String[] args) {
+		int value = 0;
+	while (value < 6) {
+		System.out.println("Mari" + " " + value);
+		value = value + 1;
+	}
+
+	}
+
+}
