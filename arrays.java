ea4e71f5ae946ee563bea142c9dd2d93c5a8c913
maricorla <55948662+maricorla@users.noreply.github.com>
2019-10-01 22:01:05 +0300

    Create arrays.java

---
+++ arrays.java
@@ -0,0 +1,12 @@
+public class Application {
+
+	public static void main(String[] args) {
+		 int[] nums = {1,2,3,4,5};
+		for (int i = 0; i < nums.length; i++)
+		{
+		  System.out.println(nums[i]);
+		}
+		
+		
+	}
+}
