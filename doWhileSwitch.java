5196c39ae86fcdb2a508d2d31823d9243d96d2df
maricorla <55948662+maricorla@users.noreply.github.com>
2019-10-01 22:51:08 +0300

    Create doWhileSwitch.java

---
+++ doWhileSwitch.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		
+		do {
+			System.out.print("Enter your command: ");
+			String text = input.nextLine();
+			
+			switch(text) {
+			 
+			 case "start":
+				 System.out.println("Machine started!");
+				 break;
+			 case "stop":
+				 System.out.println("Machine stopped!");
+				 break;
+				 
+			default:
+				System.out.println("Command doesn't recognized");
+			}
+		} while (true);
+
+	}
+}
