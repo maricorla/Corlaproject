3c0a2fc0dce5235f1370ab006cb50fa9154ffba2
maricorla <55948662+maricorla@users.noreply.github.com>
2019-10-01 23:10:29 +0300

    Update doWhileSwitch.java
    improved code with VALID Command variable

--- doWhileSwitch.java
+++ doWhileSwitch.java
@@ -6,6 +6,7 @@ public class Application {
 
 		Scanner input = new Scanner(System.in);
 		String text;
+		boolean validCommand = false;
 		
 		do {
 			System.out.print("Enter your command: ");
@@ -15,15 +16,22 @@ public class Application {
 			 
 			 case "start":
 				 System.out.println("Machine started!");
+				 validCommand = true;
 				 break;
 			 case "stop":
 				 System.out.println("Machine stopped!");
+				 validCommand = true;
+				 break;
+			 case "pause":
+				 System.out.println("Pausa!");
+				 validCommand = true;
 				 break;
 				 
 			default:
 				System.out.println("Command doesn't recognized");
+				validCommand = false;
 			}
-		} while (text.equals("start") || text.equals("stop"));
+		} while (validCommand == true);
 		
 		System.out.print("Shut down system");
 
